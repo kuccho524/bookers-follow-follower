c6da14c5bf49184c836a2f53e492d56cc6ed3008
kuccho524 <kucchachachanocha@gmail.com>
2021-05-02 06:45:02 +0000

    submission

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,28 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.find_by(book_id: book.id)
+    if comment.user == current_user
+      comment.destroy
+      redirect_back(fallback_location: root_path)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @user = @book.user
+    @book_comment = BookComment.new
+    @book_comments = @book.book_comments
   end
 
   def index
